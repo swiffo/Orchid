e9c631a9d98858f63147762cfe749644f5ff8f17
Swiffo <svegstrup@gmail.com>
2017-02-24 08:12:26 +0000

    Add data getter method for FX spot rates and a quick example thereof.

--- data.py
+++ data.py
@@ -1,8 +1,12 @@
+import collections
+
+import pandas as pd
 import pandas_datareader as pdr
+
 
 def equity_close_price(tickers, start_date, end_date):
     """
-    Return dataframe of time series of close prices for the specified tickers.
+    Return dataframe of date series of close prices for the specified tickers.
     Note that this are true close prices. Not adjusted for splits and dividends.
 
     :param tickers: String or list. One (string) or more (list) equity tickers. E.g., 'GS' or ['GS', 'JPM']
@@ -15,5 +19,49 @@ def equity_close_price(tickers, start_date, end_date):
 
     return prices
 
+def fx_spot_rates(crosses, start_date, end_date):
+    """
+    Return dataframe of date series of crosses. Index is date. Columns are given by specified
+    crosses.
+
+    :param crosses: List or single string of form CCY1/CCY2, e.g., "USD/JPY"
+    :param start_date: First date to include in data
+    :param end_date: Last date to include in data
+    :return: pandas dataframe indexed by date, columns indexed by crosses
+    """
+
+    # We have to make one data call per denominated currency (CCY2), so start by constructing
+    # a dictionary of denominated -> list of base ccys.
+    denominated_to_base_dict = collections.defaultdict(list)
+    for cross in crosses:
+        [base, denom] = currencies_from_cross(cross)
+        denominated_to_base_dict[denom].append(base)
+
+    # Retrieve the data
+    dataframes = []
+    for denom, bases in denominated_to_base_dict.items():
+        df = pdr.oanda.get_oanda_currency_historical_rates(start_date, end_date, quote_currency=denom, base_currency=bases)
+        df.rename(columns={base: '{}/{}'.format(base, denom) for base in bases})
+        dataframes.append(df)
+
+    all_rates = pd.concat(dataframes)
+    all_rates = all_rates[crosses] # reorder columns to match originally specified list of crosses
+
+    return all_rates
 
 
+def currencies_from_cross(cross):
+    """
+    Return base and denominated currency from cross as list.
+
+    :param cross: E.g., 'JPY/USD'
+    :return: 2-element list of currencies. [Base, Denominated].
+    """
+    ccys = cross.split('/')
+
+    # Should check here that all is well:
+    # ccys should be a list of length 2 with 3-character string entries
+    if len(ccys) != 2 or len(ccys[0]) != 3 or len(ccys[1]) != 3:
+        raise ValueError
+
+    return ccys
--- test.py
+++ test.py
@@ -2,12 +2,16 @@ import datetime
 import matplotlib.pyplot as plt
 import data
 
+# Show some historical equity prices
 tickers = ['GS', 'JPM', 'MS', 'DB', 'BAC', 'C']
 start = datetime.date(2007, 1, 1)
-end = datetime.date(2014, 1, 1)
+end = datetime.date(2017, 2, 23)
 
 close_prices = data.equity_close_price(tickers, start, end)
-plt.plot(close_prices)
+close_prices.plot()
 plt.show()
 
-
+# Show some historical FX rates
+fx_rates = data.fx_spot_rates(['GBP/USD', 'EUR/USD'], start, end)
+fx_rates.plot()
+plt.show()
