2335d7612f4c566afaf08677ccd9c34499c9dc02
Swiffo <svegstrup@gmail.com>
2017-02-26 13:57:00 +0000

    New calculation of adjusted close. Also does not work but at least it's new.

--- what is adjusted close.py
+++ what is adjusted close.py
@@ -3,70 +3,72 @@ The script attempts to replicate the 'Adjusted Close' value for equity taken fro
 Yahoo Financials data.
 
 The hypothesis is that the adjusted close price is calculated as follows:
- Starting from today (the horizon), walk backwards in time keeping track of total dividend paid out.
- At every step subtract the total dividend paid out from the price (the reason being that
- the price at that time has that dividend factored in unlike the price at the horizon). Whenever
- a dividend is encountered, add that to the total dividend paid out. Whenever a split is encountered
- (say an N to K split), multiply all earlier close prices and dividends by N/K.
+Starting from today, the horizon, walk backwards in time and whenever coming across
+a corporate action do the following:
 
- 'Butt, butt, butt ...,' I hear you cry. 'That would mean stock prices can go negative if subsequent
-  dividends sum up to more than the stock price!' You are right of course. That clearly makes it incorrect.
+DIVIDEND PAYOUT:
+ Given a payout of dividend D at time T with a close price of P. Assuming the dividend
+ was fully expected that means that price P is D lower than it otherwise would have been.
+ Thus it is a factor P/(P+D) lower than it otherwise would have been on day D. Multiply all
+ prices before day D with the factor P/(P+D).
+
+STOCK SPLIT:
+ Given a stock split of N to K at time T. A single stock at time T (and forward) is thus equivalent
+ to N/K stocks at time T-1. Or conversely, stock before time T is worth K/N of stock at time T.
+ Hence, we multiply price before time T by the factor N/K.
+
+
+CONCLUSION:
+ Well, the calculated adjusted close does not match the adjusted close from Yahoo, so who knows ...
 """
 
 import datetime
 
 import matplotlib.pyplot as plt
-import pandas as pd
 import pandas_datareader as pdr
 
 import data
 
 # Parameters
 ticker = 'C'
-start = datetime.date(2000, 1, 1)
-end = datetime.date(2017, 2, 20)
+start = datetime.date(2000, 5, 1)
+end = datetime.date(2017, 12, 31) # This needs to be day's date or later
 
-# The below is a fine example of how not to store and manipulate data.
-# Really everything should be in dataframes and the column names should be
-# tickers.
-
+# Grab the Yahoo data
 actual_close = data.equity_close_prices(ticker, start, end)
-adjusted_close = pdr.DataReader(ticker, 'yahoo', start, end)["Adj Close"]  # Pandas Series
+adjusted_close = pdr.DataReader(ticker, 'yahoo', start, end)["Adj Close"]
 all_actions = pdr.DataReader(ticker, 'yahoo-actions', start, end) # splits and dividends
 
-# Create dividends dataframe. Again, there is no point in calling the column 'dividend'.
-# It's already implied by the variable name that it contains dividends. Much better would
-# be to allow for multiple tickers and use the tickers as column names.
-dividends = all_actions[all_actions.action == 'DIVIDEND'][['value']]
-dividends.rename(columns={'value': 'dividend'}, inplace=True)
+# Create the factors to divide historical actual close price by due to dividend effects.
+# When a dividend D is paid out on day T, the actual close on day D, call it C, will be
+# D lower than it would have been without dividend (if the dividend was fully priced in).
+# Hence, price is lowered by a factor C/(C+D) on that day. Thus, we should divide closes
+# on all previous days with that factor.
+dividends = all_actions[all_actions.action == 'DIVIDEND'].value.copy()
+actual_close_on_dividend_dates = actual_close[dividends.index]
 
-# Create splits multiplier to be used for past stock prices.
+dividend_factors = (actual_close_on_dividend_dates + dividends) / actual_close_on_dividend_dates
+dividend_factors.sort_index(inplace=True) # ascending order
+dividend_factors = dividend_factors[::-1].cumprod()[::-1]
+dividend_factors = dividend_factors.reindex(actual_close.index, method='bfill', fill_value=1)
+dividend_factors = dividend_factors.shift(-1).fillna(1)
+
+# Create splits multiplier to be used for past stock prices. A split on day T has already been factored
+# into the close price that day. That is, a 10 to 1 split on day D will see price about 10-double from
+# day D-1 to D.
 # E.g., if the stock split 2 to 1 at time T0 and 1 to 3 at time T1 > T0, then the multiplier should be:
 # 1 for T1 <= T
-# 1/3 for T0 < T < T1
-# 2/3 for T <= T0
-splits = all_actions[all_actions.action == 'SPLIT'][['value']]
-splits.rename(columns={'value': 'split'}, inplace=True)
-splits.sort_index(inplace=True)
-splits['split'] = splits.ix[::-1, 'split'].cumprod()[::-1]
-splits = splits.reindex(pd.date_range(start, end), method='bfill', fill_value=1)
+# 1/3 for T0 <= T < T1
+# 2/3 for T < T0
+split_factors = all_actions[all_actions.action == 'SPLIT'].value.copy()
+split_factors.sort_index(inplace=True) # ascending order
+split_factors = split_factors[::-1].cumprod()[::-1]
+split_factors = split_factors.reindex(actual_close.index, method='bfill', fill_value=1)
+split_factors = split_factors.shift(-1).fillna(1)
 
-# Adjust dividends for splits
-adjusted_dividends = pd.DataFrame()
-adjusted_dividends['dividend'] = dividends.dividend * splits.split
-adjusted_dividends.dropna(inplace=True)
-
-# Adjust close prices for splits
-my_adjusted_closes = pd.DataFrame()
-my_adjusted_closes['close'] = actual_close * splits.split
-my_adjusted_closes.dropna(inplace=True)
-
-# Calculate estimate of the adjusted close price
-adjusted_dividends['cumulative'] = adjusted_dividends.ix[::-1, 'dividend'].cumsum()[::-1]
-price_correction = adjusted_dividends.reindex(my_adjusted_closes.index, method='bfill', fill_value=0)
-my_adjusted_closes['close'] = my_adjusted_closes['close'] - price_correction['cumulative']
+calculated_adjusted_close = actual_close * split_factors / dividend_factors
 
 # Plot it all
-my_adjusted_closes.plot()
+calculated_adjusted_close.plot()
 adjusted_close.plot()
 plt.show()
