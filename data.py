1f93a6fb591baab9c63e190a4309765de72b0086
Swiffo <svegstrup@gmail.com>
2017-03-03 15:40:33 +0000

    Fix bug in fx_spot_rates. The Oanda framework takes it upon itself to return a time series rather than a dataframe when there is only one base currency. Very helpful if you are using it interactively; not so helpful if you are actually writing lasting code.

--- data.py
+++ data.py
@@ -40,9 +40,16 @@ def fx_spot_rates(crosses, start_date, end_date):
     # Retrieve the data
     dataframes = []
     for denom, bases in denominated_to_base_dict.items():
-        df = pdr.oanda.get_oanda_currency_historical_rates(start_date, end_date, quote_currency=denom, base_currency=bases)
-        df.rename(columns={base: '{}/{}'.format(base, denom) for base in bases})
-        dataframes.append(df)
+        spot_rates = pdr.oanda.get_oanda_currency_historical_rates(start_date, end_date, quote_currency=denom, base_currency=bases)
+        # We need to construct a dataframe with the correct column names (crosses). If there is only a single base currency,
+        # the above call returns a time series (and we have to construct a dataframe). If there are multiple base
+        # currencies, it returns a dataframe and all we need to do is rename the columns.
+        if len(bases) == 1:
+            spot_rates = pd.DataFrame(spot_rates, columns=['{}/{}'.format(base, denom) for base in bases])
+        else:
+            spot_rates.rename(columns={base: '{}/{}'.format(base, denom) for base in bases})
+
+        dataframes.append(spot_rates)
 
     all_rates = pd.concat(dataframes)
     all_rates = all_rates[crosses] # reorder columns to match originally specified list of crosses
@@ -62,6 +69,6 @@ def currencies_from_cross(cross):
     # Should check here that all is well:
     # ccys should be a list of length 2 with 3-character string entries
     if len(ccys) != 2 or len(ccys[0]) != 3 or len(ccys[1]) != 3:
-        raise ValueError
+        raise ValueError('Incorrect form of cross: {}'.format(cross))
 
     return ccys
