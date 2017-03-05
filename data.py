748f28e460d8a43af6f640c8ac09512976a37c3f
Swiffo <svegstrup@gmail.com>
2017-03-05 09:53:42 +0000

    Rename variable 'denom' to 'quote' as 'quote currency' is a standard name for what it represents.

--- data.py
+++ data.py
@@ -34,20 +34,21 @@ def fx_spot_rates(crosses, start_date, end_date):
     # a dictionary of denominated -> list of base ccys.
     denominated_to_base_dict = collections.defaultdict(list)
     for cross in crosses:
-        [base, denom] = currencies_from_cross(cross)
-        denominated_to_base_dict[denom].append(base)
+        [base, quote] = currencies_from_cross(cross)
+        denominated_to_base_dict[quote].append(base)
 
     # Retrieve the data
     dataframes = []
-    for denom, bases in denominated_to_base_dict.items():
-        spot_rates = pdr.oanda.get_oanda_currency_historical_rates(start_date, end_date, quote_currency=denom, base_currency=bases)
+    for quote, bases in denominated_to_base_dict.items():
+        spot_rates = pdr.oanda.get_oanda_currency_historical_rates(start_date, end_date, quote_currency=quote,
+                                                                   base_currency=bases)
         # We need to construct a dataframe with the correct column names (crosses). If there is only a single base currency,
         # the above call returns a time series (and we have to construct a dataframe). If there are multiple base
         # currencies, it returns a dataframe and all we need to do is rename the columns.
         if len(bases) == 1:
-            spot_rates = pd.DataFrame(spot_rates, columns=['{}/{}'.format(base, denom) for base in bases])
+            spot_rates = pd.DataFrame(spot_rates, columns=['{}/{}'.format(base, quote) for base in bases])
         else:
-            spot_rates.rename(columns={base: '{}/{}'.format(base, denom) for base in bases})
+            spot_rates.rename(columns={base: '{}/{}'.format(base, quote) for base in bases})
 
         dataframes.append(spot_rates)
 
@@ -59,7 +60,7 @@ def fx_spot_rates(crosses, start_date, end_date):
 
 def currencies_from_cross(cross):
     """
-    Return base and denominated currency from cross as list.
+    Return base and quote currency from cross as list: [base, quote].
 
     :param cross: E.g., 'JPY/USD'
     :return: 2-element list of currencies. [Base, Denominated].
